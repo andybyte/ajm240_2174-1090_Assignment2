eca70a1e4650851fc9700183ee96889134c7c37a
andybyte <andymrkva@Andys-MacBook-Pro.local>
2017-02-14 00:37:22 -0500

    Got Movement working

--- AndyVsZombies/Assets/Scripts/PlayerController.cs
+++ AndyVsZombies/Assets/Scripts/PlayerController.cs
@@ -19,6 +19,25 @@ public class PlayerController : MonoBehaviour {
 
 	void Update () {
 		Vector3 move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0.0f);
+
+		// Prevent player from moving off the level.
+
+		if (player.position.x < -9.4f & move.x < 0.0f) {
+			move.x = 0.0f;
+//			transform.position += move * speed * Time.deltaTime;
+		} 
+		if (player.position.x > 9.21f & move.x > 0.0f) {
+			move.x = 0.0f;
+//			transform.position += move * speed * Time.deltaTime;
+		} 
+		if (player.position.y > 4.1f & move.y > 0.0f) {
+			move.y = 0.0f;
+//			transform.position += move * speed * Time.deltaTime;
+		} 
+		if (player.position.y < -3.83f & move.y < 0.0f) {
+			move.y = 0.0f;
+		}
+
 		transform.position += move * speed * Time.deltaTime;
 
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
